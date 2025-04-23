81d3d1becb973639569a079d3a76cf9b72560d0f
Shilei Tian <i@tianshilei.me>
2025-04-23 09:03:45 -0400

    [AMDGPU][Verifier] Check address space of `alloca` instruction

--- llvm/lib/IR/Verifier.cpp
+++ llvm/lib/IR/Verifier.cpp
@@ -4392,6 +4392,12 @@ void Verifier::visitAllocaInst(AllocaInst &AI) {
     verifySwiftErrorValue(&AI);
   }
 
+  if (TT.isAMDGPU()) {
+    Check(AI.getAddressSpace() == AMDGPUAS::PRIVATE_ADDRESS ||
+              AI.getAddressSpace() == AMDGPUAS::FLAT_ADDRESS,
+          "alloca on amdgpu must be in addrspace(0) or addrspace(5)", &AI);
+  }
+
   visitInstruction(AI);
 }
 
