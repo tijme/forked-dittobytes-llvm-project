e37af4de49875f610584066c54323d5c57938471
Tijme Gommers <tijme@users.noreply.github.com>
2025-05-09 22:47:09 +0200

    Update RegAllocFast.cpp

--- llvm/lib/CodeGen/RegAllocFast.cpp
+++ llvm/lib/CodeGen/RegAllocFast.cpp
@@ -880,34 +880,34 @@ void RegAllocFast::allocVirtReg(MachineInstr &MI, LiveReg &LR, Register Hint0,
                     << " in class " << TRI->getRegClassName(&RC)
                     << " with hint " << printReg(Hint0, TRI) << '\n');
 
-if (FastRandomizeRegisterAllocation) {
-    // Initialize vector of registers
-    std::vector<MCPhysReg> AllRegisters;
-    ArrayRef<MCPhysReg> Order = RegClassInfo.getOrder(&RC);
-
-    for (auto I = Order.begin(), E = Order.end(); I != E; ++I) {
-        AllRegisters.push_back(*I);
-    }
-
-    // Initialize a random number engine
-    std::random_device rd;
-    std::mt19937 gen(rd());
-
-    // Shuffle the vector
-    std::shuffle(AllRegisters.begin(), AllRegisters.end(), gen);
-
-    // Create a new ArrayRef from the shuffled vector
-    ArrayRef<MCPhysReg> RandomOrder(AllRegisters);
-
-    for (auto I = RandomOrder.begin(), E = RandomOrder.end(); I != E; ++I) {
-      assert(*I);
-
-      if (isPhysRegFree(*I)) {
-        assignVirtToPhysReg(MI, LR, *I);
-        return;
+  if (FastRandomizeRegisterAllocation) {
+      // Initialize vector of registers
+      std::vector<MCPhysReg> AllRegisters;
+      ArrayRef<MCPhysReg> Order = RegClassInfo.getOrder(&RC);
+  
+      for (auto I = Order.begin(), E = Order.end(); I != E; ++I) {
+          AllRegisters.push_back(*I);
+      }
+  
+      // Initialize a random number engine
+      std::random_device rd;
+      std::mt19937 gen(rd());
+  
+      // Shuffle the vector
+      std::shuffle(AllRegisters.begin(), AllRegisters.end(), gen);
+  
+      // Create a new ArrayRef from the shuffled vector
+      ArrayRef<MCPhysReg> RandomOrder(AllRegisters);
+  
+      for (auto I = RandomOrder.begin(), E = RandomOrder.end(); I != E; ++I) {
+        assert(*I);
+  
+        if (isPhysRegFree(*I)) {
+          assignVirtToPhysReg(MI, LR, *I);
+          return;
+        }
       }
     }
-  }
 
   // Take hint when possible.
   if (Hint0.isPhysical() && MRI->isAllocatable(Hint0) && RC.contains(Hint0) &&
